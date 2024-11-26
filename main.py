269ef2f11a2b9bb396c306643566e70a85510219
semihbag <semih702bag@gmail.com>
2024-11-26 13:34:39 +0300

    read json

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello babuş")
+import json
+import os
+import traceback
+
+current_dir = os.path.dirname(__file__)
+path = os.path.join(current_dir,"resources", "original_train_dataset.json")
+
+try:
+    with open(path, "r", encoding="utf-8") as dosya:
+        veriler = json.load(dosya)
+        
+except Exception as e:
+    print(traceback.format_exc())
